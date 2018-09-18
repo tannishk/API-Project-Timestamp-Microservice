e913cfc830efc630319f72e49f23d64b2b730998
Glitch (husky-hoe) <none>
2018-09-18 14:15:49 +0000

    🌷🔖 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_string",function(req,res){
+  if(+req.params.date_string){
+    var date = new Date(+req.params.date_string);
+  } else {
+    var date = new Date(req.params.date_string);
+  } 
+  console.log(date);
+  res.json({"unix":date.getTime(),"utc":date.toUTCString()});  
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
